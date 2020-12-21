923bfba961fa05e3fb3d366a02d257065a33e218
Rinku97 <rinkurb0@gmail.com>
2020-12-22 05:24:23 +0530

    added host

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x-x#pfr^rkatdzph#cu@kz#gqpxp_wu^yxf9_9j^ntzudm4(fw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
